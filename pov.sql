b1653c9c3db482c42790a3fb39824e52ce224da9
Luís Martins <48367937+43Clock@users.noreply.github.com>
2020-12-23 13:34:09 +0000

    Add files via upload

--- pov.sql
+++ pov.sql
@@ -1,5 +1,11 @@
 use armazem;
 
+insert into Palete values (1,'Arroz',3.5,3,0,1,'Arroz/3.5/3');
+insert into Palete values (2,'Pregos',4,7,0,1,'Pregos/4/7');
+insert into Palete values (3,'Cimento',13,1,0,1,'Cimento/13/1');
+insert into Palete values (4,'Ferro',23,12,0,1,'Ferro/23/12');
+insert into Palete values (5,'Esferovite',6,1,0,1,'Esferovite/6/1');
+
 insert into Prateleira (Localizacao) values (1);
 insert into Prateleira (Localizacao) values (2);
 insert into Prateleira (Localizacao) values (3);
